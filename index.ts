b7903677459d6518dd848fdfc0c9a6113ef8abdd
grom0330 <69398712+grom0330@users.noreply.github.com>
2023-02-22 19:33:22 +0300

    fix: fix cors problem

--- index.ts
+++ index.ts
@@ -2,11 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const appRouter = require('./Router.ts');
+import Express from 'express';
 
 const app = express();
 
 app.use(express.json());
 app.use('/', appRouter);
+app.use(
+  (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+    res.header({ 'Access-Control-Allow-Origin': '*' });
+    next();
+  }
+);
 
 const start = async () => {
   try {
