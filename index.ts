f7adef6d841a9c025883ab06ca01d740db2d3743
grom0330 <69398712+grom0330@users.noreply.github.com>
2023-02-24 11:23:22 +0300

    fix: fix CORS problem

--- index.ts
+++ index.ts
@@ -6,14 +6,18 @@ import Express from 'express';
 
 const app = express();
 
-app.use(express.json());
-app.use('/', appRouter);
 app.use(
   (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
     res.header({ 'Access-Control-Allow-Origin': '*' });
+    res.header({ 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PATCH' });
+    res.header({
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    });
     next();
   }
 );
+app.use(express.json());
+app.use('/', appRouter);
 
 const start = async () => {
   try {
